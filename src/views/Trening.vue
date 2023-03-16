4f364d829fe79e9284542fb1e710b52e82970244
pevukovic <pvukovic1111@gmail.com>
2023-03-16 17:56:50 +0100

    rad-na-glavnoj-stranici

---
+++ src/views/Trening.vue
@@ -0,0 +1,37 @@
+  <template>
+  <div v-cloak>
+      <ul v-if="lowerBody">
+        <li v-for="vjezba in lowerBody.vježbe" :key="vjezba.ime">
+          {{ vjezba.ime }} - {{ vjezba.serija }}x{{ vjezba.min_ponavljanja }}-{{ vjezba.max_ponavljanja }}@{{ vjezba.rpe }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        lowerBody: null,
+      };
+    },
+    mounted() {
+      fetch('http://localhost:3000/lowerbody')
+        .then(response => response.json())
+        .then(data => {
+          console.log("objekt");
+          console.log(data);
+          console.log("objekt");
+          alert(data);
+          this.lowerBody = data;
+          console.log(this.lowerBody)
+        });
+    }
+  }
+  </script>
+  
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
