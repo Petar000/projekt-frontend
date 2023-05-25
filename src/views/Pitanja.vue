491407324b10cbf99c77cd8d26b3747e6ee3ae9d
pevukovic <pvukovic1111@gmail.com>
2023-05-25 04:24:55 +0200

    zadnji

--- src/views/Pitanja.vue
+++ src/views/Pitanja.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="container-fluid bg-img2 vh-100 d-flex flex-column justify-content-center align-items-center">
+  <div
+    class="container-fluid bg-img2 vh-100 d-flex flex-column justify-content-center align-items-center"
+  >
     <div class="d-flex row justify-content-start align-items-start">
-        <img src="../assets/learntotrain1.png" alt="Logo" class="trainLogo2 img-fluid" />
+      <img
+        src="../assets/learntotrain1.png"
+        alt="Logo"
+        class="trainLogo2 img-fluid"
+      />
     </div>
     <div class="row justify-content-center">
       <div class="col-12">
-        <h2 class="pitanje1"> 1. Koji dio tijela bi htio znatno više napredovati?</h2>
+        <h2 class="pitanje1">
+          1. Koji dio tijela bi htio znatno više napredovati?
+        </h2>
       </div>
       <div class="col-3">
         <button class="my-button1">Trup</button>
@@ -20,16 +28,18 @@
     </div>
     <div class="row justify-content-center">
       <div class="col-12">
-        <h2 class="pitanje2">2. Koliko puta tjedno bi htio trenirati? (preporučeno 3 ili 4)</h2>
+        <h2 class="pitanje2">
+          2. Koliko puta tjedno bi htio trenirati? (preporučeno 3 ili 4)
+        </h2>
       </div>
       <div class="col-4">
-        <button class="my-button2" style="margin-left:15vw;">2</button>
+        <button class="my-button2" style="margin-left: 15vw">2</button>
       </div>
       <div class="col-4">
         <button class="my-button2">3</button>
       </div>
       <div class="col-4">
-        <button class="my-button2" style="margin-right:15vw;">4</button>
+        <button class="my-button2" style="margin-right: 15vw">4</button>
       </div>
     </div>
     <button @click="isanswered" id="dalje">Nastavi</button>
@@ -37,14 +47,14 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data() {
     return {
       prviKliknut: false,
       drugiKliknut: false,
-      odgovori: {}
+      odgovori: {},
     };
   },
 
@@ -78,38 +88,42 @@ export default {
   },
   methods: {
     saveObject() {
-      axios.post('https://learntotrain-backend.herokuapp.com/odgovori', this.odgovori)
-        .then(response => {
-          console.log('Objekt spremljen')
+      axios
+        .put(
+          "https://learntotrain-backend.herokuapp.com/odgovori",
+          this.odgovori
+        )
+        .then((response) => {
+          console.log("Objekt ažuriran");
         })
-        .catch(error => {
-          console.error(error)
-        })
-      this.tips()
+        .catch((error) => {
+          console.error(error);
+        });
+
+      this.tips();
     },
     tips() {
       this.$router.push({ name: "Savjeti" });
       document.querySelectorAll(".my-button1").forEach((b) => {
-          b.style.backgroundColor = "#2DA6E8";
-          b.style.color = "black";
-        });
+        b.style.backgroundColor = "#2DA6E8";
+        b.style.color = "black";
+      });
       document.querySelectorAll(".my-button2").forEach((b) => {
-          b.style.backgroundColor = "#2DA6E8";
-          b.style.color = "black";
-        });
+        b.style.backgroundColor = "#2DA6E8";
+        b.style.color = "black";
+      });
     },
     isanswered() {
-      console.log(this.odgovori)
+      console.log(this.odgovori);
       if (this.prviKliknut == true && this.drugiKliknut == true) {
         this.saveObject();
       }
-    }
+    },
   },
 };
 </script>
 
 <style scoped>
-
 .my-button1 {
   font-size: 2vw;
   border-radius: 1vw;
@@ -129,39 +143,39 @@ export default {
   border-radius: 1vw;
   padding: 1.5vw 2.4vw;
 }
-.pitanje1{
+.pitanje1 {
   font-size: 3vw;
   margin-bottom: 2vw;
 }
-.pitanje2{
+.pitanje2 {
   font-size: 3vw;
   margin-top: 5vw;
   margin-bottom: 2vw;
 }
 
 @media (max-width: 768px) {
-  .pitanje1{
+  .pitanje1 {
     font-size: 6vw;
   }
-  .pitanje2{
+  .pitanje2 {
     font-size: 6vw;
   }
   .my-button1 {
-  font-size: 4vw;
-  border-radius: 2vw;
-  padding: 3vw 4vw;
-}
-.my-button2 {
-  font-size: 4vw;
-  border-radius: 2vw;
-  padding: 3vw 4vw;
-}
-#dalje{
-  bottom: 6vw;
-  right: 4.5vw;
-  font-size: 4.5vw;
-  border-radius: 3vw;
-  padding: 5vw 7vw;
-}
+    font-size: 4vw;
+    border-radius: 2vw;
+    padding: 3vw 4vw;
+  }
+  .my-button2 {
+    font-size: 4vw;
+    border-radius: 2vw;
+    padding: 3vw 4vw;
+  }
+  #dalje {
+    bottom: 6vw;
+    right: 4.5vw;
+    font-size: 4.5vw;
+    border-radius: 3vw;
+    padding: 5vw 7vw;
+  }
 }
 </style>
