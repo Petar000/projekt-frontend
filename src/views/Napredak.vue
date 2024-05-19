36dcc5aee5eeb8b2128f8ca93679514ed56fc9c3
Petar000 <pvukovic111@gmail.com>
2024-05-20 00:17:53 +0200

    radi sessionId

--- src/views/Napredak.vue
+++ src/views/Napredak.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="navbar navbar-expand-lg navbar-light traka">
     <div class="container-fluid">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <button @click="idiNaHome" id="idihome">Naslovna stranica</button>
           <li class="nav-item">
-            <a class="nav-link" href="#" @click="idiNaTrening()"
-              >Povratak na trening</a
-            >
+            <a class="nav-link" href="#" @click="idiNaTrening()">Povratak na trening</a>
           </li>
         </ul>
       </div>
     </div>
   </div>
-  <div
-    class="container-fluid vh-90 d-flex align-items-top justify-content-center"
-  >
+  <div class="container-fluid vh-90 d-flex align-items-top justify-content-center">
     <div class="container-fluid">
       <h1>Praćenje podataka o napredku</h1>
       <div style="overflow-x: auto">
@@ -49,44 +38,19 @@
                     <tr v-for="(mjera, index) in mjere.objekti" :key="index">
                       <td>Tjedan {{ index + 1 }}</td>
                       <td>
-                        <input
-                          v-model="mjera.natkoljenica"
-                          type="number"
-                          min="0"
-                          id="natkoljenica"
-                        />
+                        <input v-model="mjera.natkoljenica" type="number" min="0" id="natkoljenica" />
                       </td>
                       <td>
-                        <input
-                          v-model="mjera.bokovi"
-                          type="number"
-                          min="0"
-                          id="bokovi"
-                        />
+                        <input v-model="mjera.bokovi" type="number" min="0" id="bokovi" />
                       </td>
                       <td>
-                        <input
-                          v-model="mjera.struk"
-                          type="number"
-                          min="0"
-                          id="struk"
-                        />
+                        <input v-model="mjera.struk" type="number" min="0" id="struk" />
                       </td>
                       <td>
-                        <input
-                          v-model="mjera.prsa"
-                          type="number"
-                          min="0"
-                          id="prsa"
-                        />
+                        <input v-model="mjera.prsa" type="number" min="0" id="prsa" />
                       </td>
                       <td>
-                        <input
-                          v-model="mjera.nadlaktica"
-                          type="number"
-                          min="0"
-                          id="nadlaktica"
-                        />
+                        <input v-model="mjera.nadlaktica" type="number" min="0" id="nadlaktica" />
                       </td>
                     </tr>
                   </tbody>
@@ -94,15 +58,15 @@
               </div>
             </div>
           </div>
-        <div class="botuni">
-          <button @click="spremiMjere" class="btn btn-primary" id="spremanje">
-            Spremi mjere
-          </button>
-          <button @click="izbrisiMjere" class="btn btn-danger" id="brisanje">
-            Izbriši podatke o napretku
-          </button>
+          <div class="botuni">
+            <button @click="spremiMjere" class="btn btn-primary" id="spremanje">
+              Spremi mjere
+            </button>
+            <button @click="izbrisiMjere" class="btn btn-danger" id="brisanje">
+              Izbriši podatke o napretku
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   </div>
@@ -116,77 +80,77 @@ export default {
       mjere: {
         sessionId: this.$sessionId,
         objekti: [
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-        {
-          natkoljenica: null,
-          bokovi: null,
-          struk: null,
-          prsa: null,
-          nadlaktica: null,
-        },
-      ],
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+          {
+            natkoljenica: null,
+            bokovi: null,
+            struk: null,
+            prsa: null,
+            nadlaktica: null,
+          },
+        ],
       },
       dohvaceneMjere: [],
     };
@@ -195,23 +159,26 @@ export default {
     console.log('sessionId vrijednost: ', this.$sessionId)
     this.dohvatiMjere();
   },
+
   methods: {
-    izbrisiMjere() {
-      this.mjere.forEach((mjera) => {
+    async izbrisiMjere() {
+      this.mjere.objekti.forEach((mjera) => {
         mjera.natkoljenica = null;
         mjera.bokovi = null;
         mjera.struk = null;
         mjera.prsa = null;
         mjera.nadlaktica = null;
       });
-      axios
-        .delete("https://learntotrain-backend.onrender.com/izbrisisve")
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.error(error);
+      try {
+        const response = await axios.delete("https://learntotrain-backend.onrender.com/izbrisisve", {
+          params: {
+            sessionId: this.$sessionId
+          }
         });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     },
 
     spremiMjere() {
@@ -231,10 +198,10 @@ export default {
     async dohvatiMjere() {
       try {
         const response = await axios.get("https://learntotrain-backend.onrender.com/mjere", {
-      params: {
-        sessionId: this.$sessionId
-      }
-    });
+          params: {
+            sessionId: this.$sessionId
+          }
+        });
         this.dohvaceneMjere = response.data;
         console.log("stigle mjere", this.dohvaceneMjere);
         this.spremiUTablicu();
@@ -249,12 +216,12 @@ export default {
       const praveMjere = this.dohvaceneMjere[0].rezultati.objekti;
 
       for (let i = 0; i < this.mjere.objekti.length; i++) {
-    this.mjere.objekti[i].natkoljenica = praveMjere[i].natkoljenica;
-    this.mjere.objekti[i].bokovi = praveMjere[i].bokovi;
-    this.mjere.objekti[i].struk = praveMjere[i].struk;
-    this.mjere.objekti[i].prsa = praveMjere[i].prsa;
-    this.mjere.objekti[i].nadlaktica = praveMjere[i].nadlaktica;
-  }
+        this.mjere.objekti[i].natkoljenica = praveMjere[i].natkoljenica;
+        this.mjere.objekti[i].bokovi = praveMjere[i].bokovi;
+        this.mjere.objekti[i].struk = praveMjere[i].struk;
+        this.mjere.objekti[i].prsa = praveMjere[i].prsa;
+        this.mjere.objekti[i].nadlaktica = praveMjere[i].nadlaktica;
+      }
     },
 
     idiNaTrening() {
@@ -275,36 +242,43 @@ table {
   border-collapse: collapse;
   box-shadow: 0px 0px 10px #888;
 }
+
 .table-responsive {
   overflow-x: auto;
   overflow-y: auto;
 }
+
 .table-responsive input {
   width: 100%;
   height: 100%;
   box-sizing: border-box;
 }
+
 .botuni {
   display: flex;
   justify-content: space-evenly;
 }
+
 #brisanje {
   padding: 0.8vw;
   font-size: 1.2vw;
   font-weight: bold;
   margin-left: 1vw;
 }
+
 #spremanje {
   padding: 0.8vw;
   font-size: 1.2vw;
   font-weight: bold;
 }
+
 #idihome {
   font-weight: bold;
   border-radius: 0.5vw;
   color: white;
   background-color: #001bff;
 }
+
 #idihome:hover {
   color: lightgrey;
 }
@@ -317,15 +291,17 @@ h1 {
 }
 
 @media (max-width: 768px) {
-  h1{
+  h1 {
     font-size: 8vw;
     margin-bottom: 5vw;
   }
-  #spremanje{
+
+  #spremanje {
     font-size: 3.5vw;
     padding: 2.5vw;
   }
-  #brisanje{
+
+  #brisanje {
     font-size: 3.5vw;
     padding: 2.5vw;
   }
